46e2d4de982dd5944974628fc5f96ffff259c449
Robert Otieno <robertotieno.or@gmail.com>
2022-02-15 10:22:07 +0300

    WhatsApp Number update

--- components/socialMedia.js
+++ components/socialMedia.js
@@ -6,11 +6,11 @@ const SocialMedia = () => {
         <div className="text-roke-black font-medium">Follow us on social media</div>
         <div className="grid max-w-[300px] mt-5 grid-cols-6 gap-4 mx-auto lg:mx-0">
           <a href="https://www.facebook.com/" className="bg-facebook socialBtn facebook"><FaFacebook/></a>
-          <a href="https://twitter.com/" className="bg-twitter pt-[2px] text-[15px] socialBtn twitter"><FaTwitter/></a>
+          {/* <a href="https://twitter.com/" className="bg-twitter pt-[2px] text-[15px] socialBtn twitter"><FaTwitter/></a> */}
           <a href="https://www.instagram.com/" className="bg-instagram socialBtn instagram"><FaInstagram/></a>
-          <a href="https://www.linkedin.com/" className="bg-linkedin socialBtn linkedin"><FaLinkedin/></a>
-          <a href="https://www.youtube.com/" className="bg-youtube socialBtn youtube"><FaYoutube/></a>
-          <a href="https://whatsapp.com/" className="bg-whatsapp socialBtn whatsapp"><FaWhatsapp/></a>
+          {/* <a href="https://www.linkedin.com/" className="bg-linkedin socialBtn linkedin"><FaLinkedin/></a> */}
+          {/* <a href="https://www.youtube.com/" className="bg-youtube socialBtn youtube"><FaYoutube/></a> */}
+          <a href="https://wa.me/254797063277" className="bg-whatsapp socialBtn whatsapp"><FaWhatsapp/></a>
         </div>
     </div>
   )
