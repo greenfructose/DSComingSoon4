83e047e2e2d5af756d95a3ca689fc31ea8104983
Robert Otieno <robertotieno.or@gmail.com>
2022-02-15 10:25:11 +0300

    Grid

--- components/socialMedia.js
+++ components/socialMedia.js
@@ -4,7 +4,7 @@ const SocialMedia = () => {
   return (
     <div className="mt-7 lg:mt-12">
         <div className="text-roke-black font-medium">Follow us on social media</div>
-        <div className="grid max-w-[300px] mt-5 grid-cols-6 gap-4 mx-auto lg:mx-0">
+        <div className="grid max-w-[300px] mt-5 grid-cols-3 gap-4 mx-auto lg:mx-0">
           <a href="https://www.facebook.com/" className="bg-facebook socialBtn facebook"><FaFacebook/></a>
           {/* <a href="https://twitter.com/" className="bg-twitter pt-[2px] text-[15px] socialBtn twitter"><FaTwitter/></a> */}
           <a href="https://www.instagram.com/" className="bg-instagram socialBtn instagram"><FaInstagram/></a>
