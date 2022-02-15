534ac308d7d4dd059c33dbb179c6c2c7b3ff7c4a
Robert Otieno <robertotieno.or@gmail.com>
2022-02-15 12:19:02 +0300

    Logo image correction

--- components/logo.js
+++ components/logo.js
@@ -1,8 +1,11 @@
+import Image from "next/image"
+
 const Logo = () => {
   return (
     <div className="flex items-center space-x-3 lg:pr-16 pr-6">
       <picture>
-        <source media="(prefers-color-scheme: dark)" srcset="/assets/rokeserviceslogo-dark.svg"/>
+        <source media="(prefers-color-scheme: dark)" srcSet="/assets/rokeserviceslogo-dark.svg"/>
+        {/* <Image width={300} height={100} src="/assets/rokeserviceslogo.svg" alt="logo"/> */}
         <img className="w-[300px]" src="/assets/rokeserviceslogo.svg" alt="logo" />
       </picture>
         {/* <h2 className="text-2xl font-bold leading-6 text-gray-800">Roke Services</h2> */}
