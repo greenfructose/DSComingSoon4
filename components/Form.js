23edf620239dbe881638c57e0f2cbdb9a85cb0cf
greenfructose <info@justinturney.com>
2023-07-23 10:05:42 -0500

    Working on form submission

--- components/Form.js
+++ components/Form.js
@@ -2,7 +2,9 @@ import { useState } from "react";
 import {CgDanger} from 'react-icons/cg'
 
 const Form = () => {
+  const [name, setName] = useState("")
   const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
   const [errors, setErrors] = useState({})
   const [buttonText, setButtonText] = useState("Submit")
   const [showSuccessMessage, setShowSuccessMessage] = useState(false)
@@ -18,6 +20,16 @@ const Form = () => {
       isValid = false
     }
 
+    if (!name) {
+      tempErrors["name"] = true
+      isValid = false
+    }
+
+    if (!message) {
+      tempErrors["message"] = true
+      isValid = false
+    }
+
     setErrors({ ...tempErrors });
     console.log("Errors: ", errors);
     return isValid;
@@ -30,9 +42,11 @@ const Form = () => {
 
     if (isValidForm) {
       setButtonText("Sending...")
-      const response = await fetch('/api/sendgrid', {
+      const response = await fetch('https://formsubmit.co/a788eb697682dd4c661bb82c2b232fba', {
         body: JSON.stringify({
+          name: name,
           email: email,
+          message: message,
         }),
         headers: {
           "Content-Type": "application/json",
@@ -42,35 +56,29 @@ const Form = () => {
 
       const { error } = await response.json()
 
-      if (error) {
-        console.log(error)
-        setShowSuccessMessage(false)
-        setShowErrorMessage(true)
-        setButtonText("Subscribe")
-        setEmail("")
-        return
-      } else {
-        setShowSuccessMessage(true)
-        setShowErrorMessage(false)
-        setButtonText("Subscribe")
-        setEmail("")
-        return
-      }
-
+      setShowSuccessMessage(true)
+      setShowErrorMessage(false)
+      setButtonText("Submit")
+      setName("")
+      setEmail("")
+      setMessage("")
+      return
     }
   };
 
   return (
     <div className="mt-8 mb-0 mx-0">
       <form onSubmit={handleSubmit} className="coming-soon__form">
+        {errors?.name && (<p className="text-roke-red align-middle">Name required <CgDanger style={{display: 'inline-block'}}/></p>)} 
         {errors?.email && (<p className="text-roke-red align-middle">Correct email required <CgDanger style={{display: 'inline-block'}}/></p>)}
+        {errors?.message && (<p className="text-roke-red align-middle">Message required <CgDanger style={{display: 'inline-block'}}/></p>)}
         <div>
-        <input type="text" value={email} onChange={(e) => {setEmail(e.target.value)}} className="coming-soon__form-input" placeholder="Name"/>
+        <input type="text" value={name} onChange={(e) => {setName(e.target.value)}} className="coming-soon__form-input" placeholder="Name"/>
         </div>
         <div>
         <input type="text" value={email} onChange={(e) => {setEmail(e.target.value)}} className="coming-soon__form-input" placeholder="Email"/>
         </div>
-        <textarea rows="4" className="coming-soon__form-input" placeholder="Reason"></textarea>
+        <textarea value={message} onChange={(e) => {setMessage(e.target.value)}} rows="4" className="coming-soon__form-input" placeholder="Reason"></textarea>
         <button className="coming-soon__form-btn" type="submit">
           {buttonText}
         </button>
